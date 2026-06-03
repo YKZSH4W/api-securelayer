8342ac6ce63fe032840c064ac1c99514e9e5ace0
YKZSH4W <andresg_23@alu.uabcs.mx>
2026-06-03 16:34:49 -0700

    Changes to deploy

--- prisma/migrations/20260603233422_init/migration.sql
+++ prisma/migrations/20260603233422_init/migration.sql
@@ -80,7 +80,7 @@ CREATE TABLE `Questions` (
     `questionText` VARCHAR(191) NOT NULL,
     `explanation` VARCHAR(191) NOT NULL,
     `type` VARCHAR(191) NOT NULL,
-    `order` INTEGER NOT NULL,
+    `questionOrder` INTEGER NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
